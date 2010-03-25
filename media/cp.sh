afcc3a4dbc9095dc3cf1e2180c084fd477730247
mkton <0.mkton.0@gmail.com>
2010-03-25 17:17:17 +0100

    cleanup

--- media/cp.sh
+++
@@ -1,4 +0,0 @@
-/home/remi/dev/apache-tomcat-6.0.20/bin/shutdown.sh
-rm -R ~/dev/apache-tomcat-6.0.20/webapps/media-0.0.1-SNAPSHOT*
-cp target/media-0.0.1-SNAPSHOT.war ~/dev/apache-tomcat-6.0.20/webapps/
-/home/remi/dev/apache-tomcat-6.0.20/bin/startup.sh
